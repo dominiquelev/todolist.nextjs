309a609508b5f83fe84d8905a3c4ca4bf0a62c78
dominiquelev <dominiqueliza1@gmail.com>
2024-05-29 21:24:12 +0700

    feat: layout-register-login

--- apps/web/src/app/(root)/layout.tsx
+++ apps/web/src/app/(root)/layout.tsx
@@ -1,8 +1,7 @@
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
-import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-       <Header/>
+        <Header />
         {children}
-       <Footer/>
+        <Footer />
       </body>
     </html>
   );
