7b5f6b2f3c959a85d28188099826cff714ab5157
dominiquelev <dominiqueliza1@gmail.com>
2024-06-05 15:21:57 +0700

    fix: main page layout todo

--- apps/web/src/app/(root)/page.tsx
+++ apps/web/src/app/(root)/page.tsx
@@ -1,11 +1,37 @@
-import Image from 'next/image';
-import styles from '@/app/page.module.css';
+import { Input } from '@/components/ui/input';
 import SideBar from './components/SideBar';
+import { Checkbox } from '@/components/ui/checkbox';
 
 export default function Home() {
   return (
-    <main className="">
-      <SideBar />
+    <main className="bg-slate-400 h-screen">
+      <section className="grid grid-cols-4 ">
+        <div>
+          <SideBar />
+        </div>
+        <div className="col-span-3 px-20 m-10">
+          <div className="gap-5">
+            <h2>Today main Focus</h2>
+            <h1>Design Team Meeting</h1>
+            <Input
+              type="text"
+              placeholder=" what is your task"
+              className="rounded-md "
+            />
+          </div>
+          <div className="py-10">
+            <div className="flex items-center space-x-2 ">
+              <label
+                htmlFor="todos"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                todo list
+              </label>
+              <Checkbox id="todos" />
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
